0fe86d13a0bc1b6f3a536d1389089dd8493d4e84
shivasai8688 <149556451+shivasai8688@users.noreply.github.com>
2023-11-01 10:27:22 +0530

    Identifier

---
+++ identifier,py.py
@@ -0,0 +1,51 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py
+False
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+True
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+Traceback (most recent call last):
+  File "C:\Users\Raghu\Desktop\Python Files\zfill.py", line 3, in <module>
+    print(nor(b>a))
+NameError: name 'nor' is not defined
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+True
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
+>>> 
+Warning (from warnings module):
+  File "C:\Users\Raghu\Desktop\Python Files\zfill.py", line 2
+    print( 2not in value)
+SyntaxWarning: invalid decimal literal
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+True
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+True
+>>> 
+Warning (from warnings module):
+  File "C:\Users\Raghu\Desktop\Python Files\zfill.py", line 2
+    print( 6 is value)
+SyntaxWarning: "is" with a literal. Did you mean "=="?
+>>> 
+================= RESTART: C:\Users\Raghu\Desktop\Python Files\zfill.py ================
+False
